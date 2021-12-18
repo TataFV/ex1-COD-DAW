1cd278eaf087dd8b966d0fb1d503c167c2ccf34f
mfernandezvillanueva <mfernandezvillanueva@danielcastelao.org>
2021-12-18 14:15:24 +0100

    Cambios

--- funciones.js
+++ funciones.js
@@ -1,18 +1,18 @@
-function c(d) {
-    var c = 5 / 9 * (d - 32);
-    return c;
+function celsius(degrees) {
+    var celsius = 5 / 9 * (degrees - 32);
+    return celsius;
 }
 
 
-var df = [60, 32, 100]; var dc = []; var contador;
+var degressFahrenheit = [60, 32, 100]; var degreesCelsius = []; var contador;
 
 
 
 function b() {
     for (contador = 2; contador >= 0; contador--) {
-        document.write("Valor " + contador + " es " + df[contador] + " F ");
+        document.write("Valor " + contador + " es " + degreesFarhrenheit[contador] + ' Farhrenheit ');
 
-        document.write(" el cual es " + dc[contador] +
+        document.write(" el cual es " + degrees_celsius[contador] +
             " C");
 
         fc(dc[contador])
@@ -20,16 +20,16 @@ function b() {
 }
 
 function a() {
-    for (contador = 0; contador <= 2; contador++) { dc[contador] = c(df[contador]); }
+    for (contador = 0; contador <= 2; contador++) { dc[contador] = celsius(degreesFarhrenheit[contador]); }
 }
 
-function fc(aux) {
+function farhrenheitCalculation(aux) {
     if (aux > 25) {
-        document.write(" Hace calor <br />");
+        document.write('Hace calor <br />');
     } else
         if (aux < 15) {
-            document.write(" Hace frio <br />");
+            document.write(' Hace frio <br />');
         } else {
-            document.write(" Ni fu ni fa <br />")
+            document.write(' Ni fu ni fa <br />')
         }
 }
